245a9927924e219bed085fd428c9754ba3ec8593
YUJI takahashi <jit.y.31@gmail.com>
2017-08-20 20:09:37 +0900

    Modify handler to respond message only with own name

--- test/bun2/robot_test.exs
+++ test/bun2/robot_test.exs
@@ -1,10 +1,11 @@
 defmodule Bun2.RobotTest do
   use ExUnit.Case, async: true
-  @moduletag handlers: [TestHandler], adapter: TestAdapter
+  @moduletag handlers: [TestHandler], adapter: TestAdapter, name: "bun2"
 
   setup context do
     Application.put_env(:bun2, :handlers, context[:handlers], persistent: true)
     Application.put_env(:bun2, :adapter, context[:adapter], persistent: true)
+    Application.put_env(:bun2, :name, context[:name], persistent: true)
     {:ok, robot} = Bun2.Robot.start_link(%{})
     {:ok, %{robot: robot, state: %Bun2.Robot{}}}
   end
